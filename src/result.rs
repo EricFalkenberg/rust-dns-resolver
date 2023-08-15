a03360c4936594aa38fdf1f2701a5514f21d0ec6
Eric Falkenberg <eric.falkenberg@dfstudio.com>
2023-08-15 15:50:27 -0400

    add a couple of tests

--- src/result.rs
+++ src/result.rs
@@ -15,3 +15,52 @@ impl DNSResult {
         self.answers.len() == 0 && self.authorities.len() > 0
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::header::DNSHeader;
+    use crate::record::DNSRecord;
+    use crate::result::DNSResult;
+
+    #[test]
+    fn further_info_required() {
+        let result = DNSResult {
+            header: DNSHeader {
+                ..Default::default()
+            },
+            questions: vec![],
+            answers: vec![],
+            authorities: vec![
+                DNSRecord {
+                    name: String::from("www.google.com"),
+                    type_: 1,
+                    class_: 1,
+                    ttl: 1,
+                    data: String::from("asdfasdf")
+                }
+            ],
+            additionals: vec![],
+        };
+        assert_eq!(result.further_info_required(), true);
+    }
+    #[test]
+    fn further_info_not_required() {
+        let result = DNSResult {
+            header: DNSHeader {
+                ..Default::default()
+            },
+            questions: vec![],
+            answers: vec![
+                DNSRecord {
+                    name: String::from("www.google.com"),
+                    type_: 1,
+                    class_: 1,
+                    ttl: 1,
+                    data: String::from("asdfasdf")
+                }
+            ],
+            authorities: vec![],
+            additionals: vec![],
+        };
+    }
+}
