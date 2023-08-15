66c4803b63bf84135e5eb7135ddf4941edc8f3a7
Eric Falkenberg <eric.falkenberg@dfstudio.com>
2023-08-15 12:38:53 -0400

    move dns resolving code into its own file

--- src/main.rs
+++ src/main.rs
@@ -7,112 +7,17 @@ mod record;
 mod util;
 mod rfc_type;
 mod result;
+mod dns;
 
 use std::env;
-use std::io::Cursor;
-use std::net::UdpSocket;
-use std::ops::{DerefMut};
-use byte_string::{ByteString};
-use header::DNSHeader;
-use question::DNSQuestion;
-use rand::Rng;
+use std::io::{Error};
 
-use crate::record::DNSRecord;
-use crate::result::DNSPacket;
-use crate::rfc_type::ClassType;
-use crate::rfc_type::RecordType;
-
-fn resolve(domain_name: &String) -> DNSPacket {
-    let nameserver_ip = String::from("198.41.0.4");
-    _resolve(domain_name, &nameserver_ip)
-}
-
-fn _resolve(domain_name: &String, nameserver_ip: &String) -> DNSPacket {
-    println!("Querying DNS for: {0} at nameserver address {1}", domain_name, nameserver_ip);
-    let query = build_dns_query(domain_name, RecordType::A);
-    let response = send_dns_query(query, nameserver_ip);
-    if response.answers.len() == 0 {
-        let nameserver_name = &response.authorities.get(0).unwrap().data;
-        for entry in &response.additionals {
-            if entry.type_ == RecordType::A as u16 && &entry.name == nameserver_name {
-                return _resolve(domain_name, &entry.data);
-            }
-        }
-        response
-    } else {
-        response
-    }
-}
-
-fn build_dns_query(domain_name: &String, record_type: RecordType) -> ByteString {
-    let mut rng = rand::thread_rng();
-    let header = DNSHeader {
-        id: rng.gen_range(0..65535),
-        num_questions: 1,
-        ..Default::default()
-    };
-    let question = DNSQuestion {
-        name: String::from(domain_name),
-        type_: record_type as u16,
-        class_: ClassType::IN as u16
-    };
-    let mut output = header.to_bytes();
-    output.append(question.to_bytes().deref_mut());
-    output
-}
-
-fn send_dns_query(query: ByteString, nameserver_ip: &String) -> DNSPacket {
-    let mut buf = [0; 1024];
-    let socket = UdpSocket::bind("0.0.0.0:0")
-        .expect("could not bind this address");
-    let mut ip = String::from(nameserver_ip);
-    ip.push_str(":53");
-    socket.send_to(query.as_slice(), ip)
-        .expect("could not send bytes");
-    let response_size = socket.recv(&mut buf).unwrap();
-    let response_vector = Vec::from(&buf[0..response_size]);
-    read_results(ByteString::from(response_vector))
-}
-
-fn read_results(response: ByteString) -> DNSPacket {
-    let mut questions = Vec::new();
-    let mut answers = Vec::new();
-    let mut authorities = Vec::new();
-    let mut additionals = Vec::new();
-    let mut cursor = Cursor::new(response);
-    let header = DNSHeader::parse_from_bytes(&mut cursor);
-    for _ in 0..header.num_questions {
-        let result = DNSQuestion::parse_from_bytes(&mut cursor);
-        questions.push(result);
-    }
-    for _ in 0..header.num_answers {
-        let record = DNSRecord::parse_from_response(&mut cursor);
-        answers.push(record);
-    }
-    for _ in 0..header.num_authorities {
-        let record = DNSRecord::parse_from_response(&mut cursor);
-        authorities.push(record);
-    }
-    for _ in 0..header.num_additionals {
-        let record = DNSRecord::parse_from_response(&mut cursor);
-        additionals.push(record);
-    }
-    DNSPacket {
-        header,
-        questions,
-        answers,
-        authorities,
-        additionals
-    }
-}
-
-fn main() {
+fn main() -> Result<(), Error> {
     let args: Vec<String> = env::args().collect();
     let default = String::from("example.com");
     let domain = args.get(1).unwrap_or(&default);
-    println!("Querying DNS for: {:?}", domain);
-    let results = resolve(domain);
-    // let query = build_dns_query(domain, RecordType::A);
-    // let results = send_dns_query(query);
+    println!("Querying DNS for: {0}", domain);
+    let results = dns::resolve(domain)?;
     println!("{:#?}", results);
+    Ok(())
 }
